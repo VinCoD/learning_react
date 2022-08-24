1da2a0aa92bab717fd0fc4d5a2985e5c1fcfc680
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-24 13:01:18 +0300

    Initial on template literals

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const main = {
+  color: "white",
+  background: "green"
+}
+
+const first = "Expert"
+const second = "Developer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1 style={{color:"red"}}>Hello World</h1>
+  <>
+    <h1 style={main}>Hello World</h1>
+    <p>I am an {first}</p>
+    <p>{second}</p>
+  
+  </>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
