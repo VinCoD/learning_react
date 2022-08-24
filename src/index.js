3199701b6e88521c78493b3728ba8f1b4c17fa6a
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-24 12:56:17 +0300

    Inline css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello World</h1>
+  <h1 style={{color:"red"}}>Hello World</h1>
 );
 
 // If you want to start measuring performance in your app, pass a function
